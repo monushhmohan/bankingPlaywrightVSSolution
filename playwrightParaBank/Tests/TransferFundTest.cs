964c73cd64010a04755130940efb68e648781a6b
Mohan Chantada <m.b.chantada@outlook.com>
2024-09-07 20:38:15 +0100

    Added tranfer fund test

---
+++ playwrightParaBank/Tests/TransferFundTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Playwright;
+
+namespace playwrightParaBank.Tests
+{
+
+    public class TransferFundTest: BaseClass
+    {
+        [Test]
+        public async Task TransferFunds_test003(){
+            await page.Locator("#leftPanel").GetByRole(AriaRole.Link, new() { Name = "Transfer Funds" }).ClickAsync();
+            await page.Locator("#amount").ClickAsync();
+            await page.Locator("#amount").FillAsync("100");
+            await page.GetByRole(AriaRole.Button, new() { Name = "Transfer" }).ClickAsync();
+            await page.GetByRole(AriaRole.Heading, new() { Name = "Transfer Complete!" }).ClickAsync();
+        }
+    }
+}
+
