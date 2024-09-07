cfdbdfde3cd86ed622ecec02a00dbc7305b78a78
Mohan Chantada <m.b.chantada@outlook.com>
2024-09-07 21:35:59 +0100

    removed a redundant line from test a

--- playwrightParaBank/Tests/TransferFundTest.cs
+++ playwrightParaBank/Tests/TransferFundTest.cs
@@ -9,7 +9,6 @@ namespace playwrightParaBank.Tests
         [Test]
         public async Task TransferFunds_test003(){
             await page.Locator("#leftPanel").GetByRole(AriaRole.Link, new() { Name = "Transfer Funds" }).ClickAsync();
-            await page.Locator("#amount").ClickAsync();
             await page.Locator("#amount").FillAsync("100");
             await page.GetByRole(AriaRole.Button, new() { Name = "Transfer" }).ClickAsync();
             await page.GetByRole(AriaRole.Heading, new() { Name = "Transfer Complete!" }).ClickAsync();
