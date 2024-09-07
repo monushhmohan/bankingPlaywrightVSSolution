485eab1f79fcb81242abd8fc17685c103f0e2a19
Mohan Chantada <m.b.chantada@outlook.com>
2024-09-07 22:05:40 +0100

    added HomePage Class to store home page objects and actions

---
+++ playwrightParaBank/Pages/HomePage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Playwright;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace playwrightParaBank.Pages
+{
+    public class HomePage
+    {
+        private readonly IPage page;
+        public HomePage(IPage page)
+        {
+            this.page = page;
+        }
+
+        // Selectors for the elements
+        private ILocator AccountServicesLink => page.GetByRole(AriaRole.Heading, new() { Name = "Account Services" });
+        private ILocator OpenNewAccountLink => page.GetByRole(AriaRole.Link, new() { Name = "Open New Account" });
+
+        // Methods for actions on the page
+        public async Task ClickOnOpenANewAccount()
+        {
+            await AccountServicesLink.ClickAsync();
+            await OpenNewAccountLink.ClickAsync();
+        }
+    }
+}
+
