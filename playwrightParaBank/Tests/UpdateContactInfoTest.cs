b129f289196bc209edd60cc763ac328c9ab19597
Mohan Chantada <m.b.chantada@outlook.com>
2024-09-30 18:30:07 +0100

    removed redundant code from test004

--- playwrightParaBank/Tests/UpdateContactInfoTest.cs
+++ playwrightParaBank/Tests/UpdateContactInfoTest.cs
@@ -13,11 +13,9 @@ namespace playwrightParaBank.Tests
         public async Task UpdateContactInfo_test004()
         {
             await page.GetByRole(AriaRole.Link, new() { Name = "Update Contact Info" }).ClickAsync();
-            await page.Locator("[id=\"customer\\.address\\.city\"]").ClickAsync();
-            await page.Locator("[id=\"customer\\.address\\.street\"]").ClickAsync();
-            await page.Locator("[id=\"customer\\.address\\.street\"]").FillAsync("Xyz Street");
-            await page.Locator("[id=\"customer\\.address\\.city\"]").ClickAsync();
-            await page.Locator("[id=\"customer\\.address\\.city\"]").FillAsync("Southampton");
+            await page.Locator("[id='customer.address.city']").ClickAsync();
+            await page.Locator("[id='customer.address.street']").FillAsync("Xyz Street");
+            await page.Locator("[id='customer.address.city']").FillAsync("Southampton");
             await page.GetByRole(AriaRole.Button, new() { Name = "Update Profile" }).ClickAsync();
             await page.GetByText("Your updated address and").ClickAsync();
             await page.GetByRole(AriaRole.Heading, new() { Name = "Profile Updated" }).ClickAsync();
